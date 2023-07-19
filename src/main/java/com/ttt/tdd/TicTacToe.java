e6812de3393f3bfe514003ec732be497cfc14636
Shashi <LENOVO@DESKTOP-2MREFRI>
2023-07-19 13:32:53 +0530

    Add:Create a dashboard

--- src/main/java/com/ttt/tdd/TicTacToe.java
+++ src/main/java/com/ttt/tdd/TicTacToe.java
@@ -1,5 +1,9 @@
 package com.ttt.tdd;
 
 public class TicTacToe {
+	
+	 private Character[][] board = {{'\0', '\0', '\0'},
+             						{'\0', '\0', '\0'},
+             						{'\0', '\0', '\0'}};
 
 }
