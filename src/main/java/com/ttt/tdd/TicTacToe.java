fe3ecf12f2d42ca6d2b3c6d784cc2588ec935e57
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-19 23:29:46 +0530

    Add passing test for top bottom diagonal win

--- src/main/java/com/ttt/tdd/TicTacToe.java
+++ src/main/java/com/ttt/tdd/TicTacToe.java
@@ -6,33 +6,32 @@ public class TicTacToe {
 
 	private Character[][] board = { { '\0', '\0', '\0' }, { '\0', '\0', '\0' }, { '\0', '\0', '\0' } };
 	private char lastPlayer = '\0';
-    private static final int SIZE = 3;
-
+	private static final int SIZE = 3;
 
 	public String play(int column, int row) {
 		checkAxis(column, "X value is outside the board!");
 		checkAxis(row, "Y value is outside the board!");
 		lastPlayer = playWithPlayer();
-		setField(column, row,lastPlayer);
+		setField(column, row, lastPlayer);
 		if (isWinner()) {
-            return lastPlayer + " is the Winner";
-        }
-        return "No winner";
+			return lastPlayer + " is the Winner";
+		}
+		return "No winner";
 	}
 
-	
-	 private boolean isWinner() {
-	        int playerTotal = lastPlayer * SIZE;
-	        for (int i = 0; i < SIZE; i++)
-	            if (board[0][i] + board[1][i] + board[2][i] == playerTotal ||
-	                board[i][0] + board[i][1] + board[i][2] == playerTotal) {
-	                return true;
-	            }
-	        if (board[0][0] + board[1][1] + board[2][2] == playerTotal) 
-	        	return true;
+	private boolean isWinner() {
+		int playerTotal = lastPlayer * SIZE;
+		for (int i = 0; i < SIZE; i++)
+			if (board[0][i] + board[1][i] + board[2][i] == playerTotal
+					|| board[i][0] + board[i][1] + board[i][2] == playerTotal) {
+				return true;
+			}
+		if (board[0][0] + board[1][1] + board[2][2] == playerTotal
+				|| board[2][0] + board[1][1] + board[0][2] == playerTotal)
+			return true;
 
-	        return false;
-	    }
+		return false;
+	}
 
 	private void checkAxis(int axis, String message) {
 		if (axis < 1 || axis > 3) {
@@ -40,11 +39,11 @@ public class TicTacToe {
 		}
 	}
 
-	private void setField(int column, int row,  char lastPlayer) {
+	private void setField(int column, int row, char lastPlayer) {
 		if (board[column - 1][row - 1] != '\0') {
 			throw new RuntimeException("Field is occupied!");
 		} else {
-			 board[column - 1][row - 1] = lastPlayer;
+			board[column - 1][row - 1] = lastPlayer;
 		}
 	}
 
